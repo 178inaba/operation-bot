72b2bfdd010fff1d6254f11427cf6c920f2e37cf
Masahiro Furudate <178inaba.git@gmail.com>
2024-02-17 05:12:54 +0900

    Add mysql

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import { App } from '@slack/bolt';
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  database: process.env.MYSQL_DATABASE,
+});
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -7,6 +14,13 @@ const app = new App({
 
 app.message('hello', async ({ message, say }) => {
   if (message.subtype === undefined || message.subtype === 'bot_message') {
+	const conn = await pool.getConnection();
+    const [results, fields] = await conn.query('SELECT * FROM `members`');
+
+	// TODO
+	console.log(results);
+	console.log(fields);
+
     await say(`hello <@${message.user}> !`);
   }
 });
